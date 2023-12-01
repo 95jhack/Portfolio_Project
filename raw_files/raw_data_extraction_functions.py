e633ea68e23cff21f7e72a937b352c2ecad1e302
95jhack <95jhack@gmail.com>
2023-11-30 20:45:45 -0800

    raw_data_extraction-teams_and_gamelogs

--- raw_files/raw_data_extraction_functions.py
+++ raw_files/raw_data_extraction_functions.py
@@ -1,16 +1,82 @@
-import pandas as pd 
+import pandas as pd
 
-class CsvImport():
+class CSVReader:
+    def __init__(self, file_path, column_names=None):
+        self.file_path = file_path
+        self.column_names = column_names
+        self.data = None
 
-    def __init__(self, name):
-        self.name = name
-        #self.header = header
+    def read_csv(self):
+        try:
+            if self.column_names is not None:
+                self.data = pd.read_csv(self.file_path, names=self.column_names)
+            else:
+                self.data = pd.read_csv(self.file_path)
 
-    def Csv_to_df(self):
-        df = pd.read_csv(self.name) #,self.header)
-        rows = len(df)
-        cols = len(df.columns)
-        print(f"Data from {self.name} has been read from a Raw CSV file to a Pandas Dataframe.")
-        print(f"{rows} rows were imported from the raw file.")
-        print(f"{cols} columns were imported from the raw file.")        
-        return df
+            rows = len(self.data)
+            cols = len(self.data.columns)
+            print(f"CSV file '{self.file_path}' successfully loaded.")
+            print(f"{rows} rows were imported from the raw file.")
+            print(f"{cols} columns were imported from the raw file.")   
+            return self.data
+        except FileNotFoundError:
+            print(f"Error: File '{self.file_path}' not found.")
+        except pd.errors.EmptyDataError:
+            print(f"Error: File '{self.file_path}' is empty.")
+        except pd.errors.ParserError:
+            print(f"Error: Unable to parse the CSV file '{self.file_path}'.")
+        return None
+
+    def display_data(self):
+        if self.data is not None:
+            print(self.data.head())
+        else:
+            print("Error: No data loaded. Use read_csv method first.")
+
+    def get_column_names(self):
+        if self.data is not None:
+            return list(self.data.columns)
+        else:
+            print("Error: No data loaded. Use read_csv method first.")
+            return []
+
+class DataframeGlueOps:
+    def __init__(self, df1, df2, new_column_names=None):
+        self.df1 = df1
+        self.df2 = df2
+        self.new_column_names = None
+
+    def merge_and_rename(self, df1, df2, new_column_names):
+        """
+        Update the naming of both individual dataframes. Merge the two DataFrames.
+
+        Parameters:
+        - df1, df2: pandas DataFrames
+        - new_column_names: list of str, shared column names for the merged DataFrame
+
+        Returns:
+        - merged_df: pandas DataFrame, the merged DataFrame with new column names
+        """
+        # Drop original column names
+        self.df1.columns=new_column_names
+        self.df2.columns=new_column_names
+
+        # Append DataFrames
+        merged_df = pd.concat([self.df1, self.df2])
+
+        return merged_df
+
+class CSVWriter:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def write_to_csv(self, file_name):
+        try:
+            if not file_name.endswith('.csv'):
+                file_name += '.csv'
+                
+            self.dataframe.to_csv(file_name, index=False)
+            print(f"Data written to {file_name} successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+            
--- raw_files/raw_data_manipulation.py
+++ raw_files/raw_data_manipulation.py
@@ -1,176 +1,48 @@
-from raw_data_extraction_functions import CsvImport
+from raw_data_extraction_functions import CSVReader, DataframeGlueOps, CSVWriter
+from raw_data_gamelogs_organization import gamelogs_df_column_names, merged_columns, dim_team_stats_home_col, dim_team_stats_visitors_col, fact_gamelogs_cols
 
-obj = CsvImport(
-    name = "JHack_Portfolio/raw_files/gl2022.csv"
-)
-gamelogs_df = obj.Csv_to_df()
-gamelogs_df_column_names = [
-    "date"
-    ,"game_no"
-    ,"dow"
-    ,"visiting_team"
-    ,"visiting_league"
-    ,"visiting_league_game_no"
-    ,"home_team"
-    ,"home_league"
-    ,"home_league_game_no"
-    ,"visiting_team_score"   
-    ,"home_team_score"
-    ,"length_outs"
-    ,"day_night"
-    ,"completion_info"
-    ,"forfeit_info"
-    ,"protest_info"
-    ,"park_info"
-    ,"attendance_info"
-    ,"time_min"
-    ,"visiting_line"
-    ,"home_line"
-    ,"v_AB"
-    ,"v_h"
-    ,"v_dbl"
-    ,"v_trpl"
-    ,"v_hr"
-    ,"v_rbi"
-    ,"v_sh"
-    ,"v_sf"
-    ,"v_hbp"
-    ,"v_bb"
-    ,"v_bb_int"
-    ,"v_so"
-    ,"v_sb"
-    ,"v_cs"
-    ,"v_gidp"
-    ,"v_ci"
-    ,"v_lob"
-    ,"v_ptchrs"
-    ,"v_er"
-    ,"v_er_team"
-    ,"v_wp"
-    ,"v_balk"
-    ,"v_po"
-    ,"v_asst"
-    ,"v_err"
-    ,"v_pb"
-    ,"v_dbl_def"
-    ,"v_trpl_def"
-    ,"h_AB"
-    ,"h_h"
-    ,"h_dbl"
-    ,"h_trpl"
-    ,"h_hr"
-    ,"h_rbi"
-    ,"h_sh"
-    ,"h_sf"
-    ,"h_hbp"
-    ,"h_bb"
-    ,"h_bb_int"
-    ,"h_so"
-    ,"h_sb"
-    ,"h_cs"
-    ,"h_gidp"
-    ,"h_ci"
-    ,"h_lob"
-    ,"h_ptchrs"
-    ,"h_er"
-    ,"h_er_team"
-    ,"h_wp"
-    ,"h_balk"
-    ,"h_po"
-    ,"h_asst"
-    ,"h_err"
-    ,"h_pb"
-    ,"h_dbl_def"
-    ,"h_trpl_def"
-    ,"home_ump_id"
-    ,"home_ump_name"
-    ,"1b_ump_id"
-    ,"1b_ump_name"
-    ,"2b_ump_id"
-    ,"2b_ump_name"
-    ,"3b_ump_id"
-    ,"3b_ump_name"
-    ,"lf_ump_id"
-    ,"lf_ump_name"
-    ,"rf_ump_id"
-    ,"rf_ump_name"
-    ,"v_manager_id"
-    ,"v_manager_name"
-    ,"h_manager_id"
-    ,"h_manager_name"
-    ,"w_pitcher_id"
-    ,"w_pitcher_name"
-    ,"l_pitcher_id"
-    ,"l_pitcher_name"
-    ,"sv_pitcher_id"
-    ,"sv_pitcher_name"
-    ,"gw_rbi_player_id"
-    ,"gw_rbi_player_name"
-    ,"v_sp_id"
-    ,"v_sp_name"
-    ,"h_sp_id"
-    ,"h_sp_name"
-    ,"v_1_id"
-    ,"v_1_name"
-    ,"v_1_pos"
-    ,"v_2_id"
-    ,"v_2_name"
-    ,"v_2_pos"
-    ,"v_3_id"
-    ,"v_3_name"
-    ,"v_3_pos"
-    ,"v_4_id"
-    ,"v_4_name"
-    ,"v_4_pos"
-    ,"v_5_id"
-    ,"v_5_name"
-    ,"v_5_pos"
-    ,"v_6_id"
-    ,"v_6_name"
-    ,"v_6_pos"
-    ,"v_7_id"
-    ,"v_7_name"
-    ,"v_7_pos"
-    ,"v_8_id"
-    ,"v_8_name"
-    ,"v_8_pos"
-    ,"v_9_id"
-    ,"v_9_name"
-    ,"v_9_pos"
-    ,"h_1_id"
-    ,"h_1_name"
-    ,"h_1_pos"
-    ,"h_2_id"
-    ,"h_2_name"
-    ,"h_2_pos"
-    ,"h_3_id"
-    ,"h_3_name"
-    ,"h_3_pos"
-    ,"h_4_id"
-    ,"h_4_name"
-    ,"h_4_pos"
-    ,"h_5_id"
-    ,"h_5_name"
-    ,"h_5_pos"
-    ,"h_6_id"
-    ,"h_6_name"
-    ,"h_6_pos"
-    ,"h_7_id"
-    ,"h_7_name"
-    ,"h_7_pos"
-    ,"h_8_id"
-    ,"h_8_name"
-    ,"h_8_pos"
-    ,"h_9_id"
-    ,"h_9_name"
-    ,"h_9_pos"
-    ,"additional_info"
-    ,"acquisition_info"
-]
-gamelogs_df.columns = gamelogs_df_column_names
+csv_reader = CSVReader("JHack_Portfolio/raw_files/gl2022.csv", column_names=gamelogs_df_column_names)
+df = csv_reader.read_csv()
 
-#print(gamelogs_df.head(1))
+# Display the data, that was read into df
+# csv_reader.display_data()
 
-teams_df = gamelogs_df[["home_team"]].drop_duplicates(["home_team"]).sort_values(["home_team"]).reset_index()
-teams_df = teams_df["home_team"]
-print(teams_df.head(30))
+columns = csv_reader.get_column_names()
+#print(f"Column Names: {columns}")
+
+#########################################################
+# Fact_gamelogs
+#########################################################
+
+fact_gamelogs = df[fact_gamelogs_cols]
+
+# Function to compare home_team_score against visiting_team_score
+def compare_scores(row):
+    if row['home_team_score'] > row['visiting_team_score']:
+        return row['home_team']
+    else:
+        return row['visiting_team']
+    
+# Apply the function to create a new column 'Result'
+fact_gamelogs['winning_team'] = fact_gamelogs.apply(compare_scores, axis=1)
+
+df_to_csv_fact_gamelogs = CSVWriter(fact_gamelogs)
+df_to_csv_fact_gamelogs.write_to_csv('JHack_Portfolio/cleaned_files/fact_gamelogs')
+
+#########################################################
+# Dim_Team_statistics
+#########################################################
+
+dim_team_statistics_visitors = df[dim_team_stats_visitors_col].groupby(by=["visiting_team","visiting_league"], as_index=False).sum()
+# print(dim_team_statistics_visitors.head(30))
+
+dim_team_statistics_home = df[dim_team_stats_home_col].groupby(by=["home_team","home_league"], as_index=False).sum()
+
+df_ops = DataframeGlueOps(df1=dim_team_statistics_home,df2=dim_team_statistics_visitors)
+dim_team_statistics_final = df_ops.merge_and_rename(df1=dim_team_statistics_home,df2=dim_team_statistics_visitors, new_column_names = merged_columns)
+
+dim_team_statistics_final = dim_team_statistics_final.groupby(by=['team','league'], as_index=False).sum()
+#print(dim_team_statistics_final.head(30))
+
+df_to_csv_dim_team_statistics = CSVWriter(dim_team_statistics_final)
+df_to_csv_dim_team_statistics.write_to_csv('JHack_Portfolio/cleaned_files/dim_team_statistics')
